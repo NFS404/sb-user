fc7ffa1c290496fd2c5b008e94c6fc017deebcf1
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-17 15:24:34 -0400

    repository path

--- src/main/java/io/github/sbcloudrace/sbuser/user/SbUserRepository.java
+++ src/main/java/io/github/sbcloudrace/sbuser/user/SbUserRepository.java
@@ -2,7 +2,9 @@ package io.github.sbcloudrace.sbuser.user;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "sbusers")
 public interface SbUserRepository extends PagingAndSortingRepository<SbUser, Long> {
 
     SbUser findByEmail(@Param("email") String email);
