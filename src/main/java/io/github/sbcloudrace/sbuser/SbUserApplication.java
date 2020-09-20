a8b89bb87decd74cb9e02e3ddb7b92f020067f03
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-20 10:27:34 -0400

    eureka setup

--- src/main/java/io/github/sbcloudrace/sbuser/SbUserApplication.java
+++ src/main/java/io/github/sbcloudrace/sbuser/SbUserApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@Enableeu
 public class SbUserApplication {
 
     public static void main(String[] args) {
